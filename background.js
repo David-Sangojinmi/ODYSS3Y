8de05da017e274739a93f1a00213c4d3469adb54
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-24 22:48:26 +0100

    Improved background layering

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ export default class Background {
         this.layer3.src = "images/bgLayer3.png";
         this.layer4.src = "images/bgLayer4.png";
 
-        this.pos = {
+        this.posGS = {
             l0x: 0,
             l1x: 0,
             l2x: 0,
@@ -19,45 +19,54 @@ export default class Background {
             l4x: 0,
             ly: 0,
         };
+        this.posGP = {
+            l0x: 0,
+            l1x: -800,
+            l2x: -800,
+            l3x: -800,
+            l4x: -800,
+            ly: 0,
+        };
     }
 
     gsdraw(ctx) {
         ctx.fillStyle = "#aedecb";
-        ctx.fillRect(this.pos.l0x, this.pos.ly, this.gameWidth, this.gameHeight);
-        ctx.drawImage(this.layer1, this.pos.l1x, this.pos.ly);
-        ctx.drawImage(this.layer2, this.pos.l2x, this.pos.ly);
-        ctx.drawImage(this.layer3, this.pos.l3x, this.pos.ly);
-        ctx.drawImage(this.layer4, this.pos.l4x, this.pos.ly);
+        ctx.fillRect(this.posGS.l0x, this.posGS.ly, this.gameWidth, this.gameHeight);
+        ctx.drawImage(this.layer1, this.posGS.l1x, this.posGS.ly);
+        ctx.drawImage(this.layer2, this.posGS.l2x, this.posGS.ly);
+        ctx.drawImage(this.layer3, this.posGS.l3x, this.posGS.ly);
+        ctx.drawImage(this.layer4, this.posGS.l4x, this.posGS.ly);
     }
 
     gpdraw(ctx) {
         ctx.fillStyle = "#aedecb";
-        ctx.fillRect(this.pos.l0x, this.pos.ly, this.gameWidth, this.gameHeight);
-        for (var i = 0; i < 2; i++) {
-            ctx.drawImage(this.layer1, this.pos.l1x + this.gameWidth * i, this.pos.ly);
-            ctx.drawImage(this.layer2, this.pos.l2x + this.gameWidth * i, this.pos.ly);
-            ctx.drawImage(this.layer3, this.pos.l3x + this.gameWidth * i, this.pos.ly);
-            ctx.drawImage(this.layer4, this.pos.l4x + this.gameWidth * i, this.pos.ly);
-        }
-        if (this.pos.l1x < -800) {
-            this.pos.l1x = 800;
-        } else if (this.pos.l1x > 800) {
-            this.pos.l1x = -800;
-        }
-        if (this.pos.l2x < -800) {
-            this.pos.l2x = 800;
-        } else if (this.pos.l2x > 800) {
-            this.pos.l2x = -800;
-        }
-        if (this.pos.l3x < -800) {
-            this.pos.l3x = 800;
-        } else if (this.pos.l3x > 800) {
-            this.pos.l3x = -800;
-        }
-        if (this.pos.l4x < -800) {
-            this.pos.l4x = 800;
-        } else if (this.pos.l4x > 800) {
-            this.pos.l4x = -800;
+        ctx.fillRect(this.posGP.l0x, this.posGP.ly, this.gameWidth, this.gameHeight);
+        for (var i = 0; i < 4; i++) {
+            ctx.drawImage(this.layer1, this.posGP.l1x + this.gameWidth * i, this.posGP.ly);
+            ctx.drawImage(this.layer2, this.posGP.l2x + this.gameWidth * i, this.posGP.ly);
+            ctx.drawImage(this.layer3, this.posGP.l3x + this.gameWidth * i, this.posGP.ly);
+            ctx.drawImage(this.layer4, this.posGP.l4x + this.gameWidth * i, this.posGP.ly);
+
+            // if (this.posGP.l1x + this.gameWidth * i < -800) {
+            //     this.posGP.l1x = 800;
+            // } else if (this.posGP.l1x + this.gameWidth * i > 800) {
+            //     this.posGP.l1x = -800;
+            // }
+            // if (this.posGP.l2x + this.gameWidth * i < -800) {
+            //     this.posGP.l2x = 800;
+            // } else if (this.posGP.l2x + this.gameWidth * i > 800) {
+            //     this.posGP.l2x = -800;
+            // }
+            // if (this.posGP.l3x + this.gameWidth * i < -800) {
+            //     this.posGP.l3x = 800;
+            // } else if (this.posGP.l3x + this.gameWidth * i > 800) {
+            //     this.posGP.l3x = -800;
+            // }
+            // if (this.posGP.l4x + this.gameWidth * i < -800) {
+            //     this.posGP.l4x = 800;
+            // } else if (this.posGP.l4x + this.gameWidth * i > 800) {
+            //     this.posGP.l4x = -800;
+            // }
         }
     }
 
