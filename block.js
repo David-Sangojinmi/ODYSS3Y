7d4ffa3c2c5d0ebb32edccab68920345745819c1
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-29 22:01:31 +0100

    Cleaning up loose comments

--- block.js
+++ block.js
@@ -11,23 +11,28 @@ export default class Block {
     }
 
     drawBlock(ctx) {
-        if (this.s === 1 || this.i === 2) {
-            ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
+        switch (this.i) {
+            case 1:
+                ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
+                break;
+            case 2:
+                ctx.drawImage(this.terrainSet, 160, 96, 16, 16, this.x, this.y, this.w, this.h);
+                break;
+            case 3:
+                ctx.drawImage(this.terrainSet, 48, 80, 16, 16, this.x, this.y, this.w, this.h);
+                break;
+            case 4: // Ground top left
+                ctx.drawImage(this.terrainSet, 0, 80, 16, 16, this.x, this.y, this.w, this.h);
+                break;
+            case 5: // Ground bottom left
+                ctx.drawImage(this.terrainSet, 0, 96, 16, 16, this.x, this.y, this.w, this.h);
+                break;
+            case 6: // Ground top right
+                ctx.drawImage(this.terrainSet, 32, 80, 16, 16, this.x, this.y, this.w, this.h);
+                break;
+            case 7: // Ground bottom right
+                ctx.drawImage(this.terrainSet, 32, 96, 16, 16, this.x, this.y, this.w, this.h);
+                break;
         }
-        // switch(this.i) {
-        //     case 1:
-        //         ctx.drawImage(
-        //             this.terrainSet,
-        //             160,
-        //             80,
-        //             16,
-        //             16,
-        //             this.x,
-        //             this.y,
-        //             this.w,
-        //             this.h
-        //         );
-        //         break;
-        // }
     }
 }
