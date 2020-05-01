e8436b182bbd3280a782668448d3fa800807daaf
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-01 17:04:22 +0100

    Initial attempt at using inheritance

--- src/block.js
+++ src/block.js
@@ -16,44 +16,146 @@ export default class Block {
     }
 
     drawBlock(ctx) {
-        switch (this.id) {
-            case 1:
-                ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
-                break;
-            case 2:
-                ctx.drawImage(this.terrainSet, 160, 96, 16, 16, this.x, this.y, this.w, this.h);
-                break;
-            case 3:
-                ctx.drawImage(this.terrainSet, 48, 80, 16, 16, this.x, this.y, this.w, this.h);
-                break;
-            case 4: // Ground top left
-                ctx.drawImage(this.terrainSet, 0, 80, 16, 16, this.x, this.y, this.w, this.h);
-                break;
-            case 5: // Ground bottom left
-                ctx.drawImage(this.terrainSet, 0, 96, 16, 16, this.x, this.y, this.w, this.h);
-                break;
-            case 6: // Ground top right
-                ctx.drawImage(this.terrainSet, 32, 80, 16, 16, this.x, this.y, this.w, this.h);
-                break;
-            case 7: // Ground bottom right
-                ctx.drawImage(this.terrainSet, 32, 96, 16, 16, this.x, this.y, this.w, this.h);
-                break;
-            case 9: // Coin 1
-                if (this.coinActive == true) {
-                    ctx.drawImage(this.coin, this.x + 5, this.y + 5);
-                    break;
-                }
-            case 10: // Coin 2
-                if (this.coinActive == true) {
-                    ctx.drawImage(this.coin2, this.x + 5, this.y + 5);
-                    break;
-                }
-        }
+        ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
     }
 
     active(ctx, blockNum) {
         ctx.font = "15px arial";
         ctx.fillStyle = "white";
         ctx.fillText(blockNum, this.x + 8, this.y + 25);
+    }
+}
+
+export class grassBlock extends Block {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class grassTLBlock extends grassBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 0, 80, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class grassTMBlock extends grassBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class grassTRBlock extends grassBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 32, 80, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class grassBLBlock extends grassBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 0, 96, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class grassBMBlock extends grassBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 160, 96, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class grassBRBlock extends grassBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 32, 96, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class stoneBlock extends Block {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class stone1Block extends stoneBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 16, 128, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class stone2Block extends stoneBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 16, 64, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class coinBlock extends Block {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        ctx.drawImage(this.terrainSet, 160, 80, 16, 16, this.x, this.y, this.w, this.h);
+    }
+}
+
+export class coin1Block extends coinBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        if (this.coinActive == true) {
+            ctx.drawImage(this.coin, this.x + 5, this.y + 5);
+        }
+    }
+}
+
+export class coin2Block extends coinBlock {
+    constructor(x, y, w, h, id) {
+        super(x, y, w, h, id);
+    }
+
+    drawBlock(ctx) {
+        if (this.coinActive == true) {
+            ctx.drawImage(this.coin2, this.x + 5, this.y + 5);
+        }
     }
 }
