2031bb67a5eb22ddbc6624f986cf632bfefa374c
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-08 00:37:57 +0100

    Added vertical movement for enemy

--- src/enemy.js
+++ src/enemy.js
@@ -1,9 +1,11 @@
 export default class Enemy {
-    constructor(x, y, xLBound, xRBound, xSpd, ySpd) {
+    constructor(x, y, xLBound, xRBound, yTBound, yBBound, xSpd, ySpd) {
         this.x = x;
         this.y = y;
         this.xLBound = xLBound;
         this.xRBound = xRBound;
+        this.yTBound = yTBound;
+        this.yBBound = yBBound;
         this.xSpd = xSpd;
         this.ySpd = ySpd;
         this.enemySprite = new Image();
@@ -15,12 +17,21 @@ export default class Enemy {
     }
 
     patrol() {
+        // Horizontal //
         this.x -= this.xSpd;
         if (this.x >= this.xRBound) {
             this.xSpd *= -1;
         }
         if (this.x <= this.xLBound) {
             this.xSpd *= -1;
+        }
+        // Vertical //
+        this.y -= this.ySpd;
+        if (this.y <= this.yTBound) {
+            this.ySpd *= -1;
+        }
+        if (this.y >= this.yBBound) {
+            this.ySpd *= -1;
         }
     }
 }
